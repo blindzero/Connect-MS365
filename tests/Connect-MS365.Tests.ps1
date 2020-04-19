2901ea47a256a5921f397428f5ebf5d06104f539
Matthias Fleschütz <13959569+blindzero@users.noreply.github.com>
2020-04-19 16:33:58 +0200

    adding module setup tests

--- tests/Connect-MS365.Tests.ps1
+++ tests/Connect-MS365.Tests.ps1
@@ -1,9 +1,31 @@
-$ModuleManifest = "$outputModVerDir\*.psd1"
+if (!($env:BHProjectPath)) {
+    Set-BuildEnvironment -Path $PSScriptRoot\..
+}
 
+$cwd = $outputModVerDir
+$moduleName
+$ModuleManifest = "$($cwd)\$($moduleName).psd1"
+
+Describe "$moduleName Module Test" {
+    Context 'Module Setup' {
+        It "Has root module $moduleName.psm1" {
+            "$cwd\$moduleName.psm1" | Should -Exist
+        }
+        It "Has the module manifest $moduleName.psd1" {
+            "$cwd\$moduleName.psd1" | Should -Exist
+            "$cwd\$moduleName.psd1" | Should -FileContentMatch "$moduleName.psm1"
+        }
+        It "$moduleName is valid PowerShell code" {
+            $psFile = Get-Content -Path "$cwd\$moduleName.psm1" -ErrorAction Stop
+            $errors = $null
+            [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            $errors.Count | Should -Be 0
+        }
+    }
+}
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         Test-ModuleManifest -Path $ModuleManifest | Should Not BeNullOrEmpty
         $? | Should Be $true
     }
 }
-

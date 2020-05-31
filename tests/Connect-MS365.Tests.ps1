397faf034293f8befed9883aa066d815db8a2912
Matthias Fleschuetz <13959569+blindzero@users.noreply.github.com>
2020-05-31 18:26:27 +0200

    changed Be to -Be in Pester for new version

--- tests/Connect-MS365.Tests.ps1
+++ tests/Connect-MS365.Tests.ps1
@@ -134,7 +134,7 @@ Describe "$moduleName Integration Tests" -Tags ('Integration') {
     Context "Integrated Manifest Test" {
         It 'Passes Test-ModuleManifest' {
             Test-ModuleManifest -Path $ModuleManifest | Should Not BeNullOrEmpty
-            $? | Should Be $true
+            $? | Should -Be $true
         }
     }
     Context "Generated ExternalHelp XML Tests" {

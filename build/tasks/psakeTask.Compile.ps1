795e183c4d133cb1539040bc5707016bc7a5da5e
Matthias Fleschuetz <13959569+blindzero@users.noreply.github.com>
2020-05-31 13:54:41 +0200

    move module main to public and split footer and header; change compile task accordingly

--- build/tasks/psakeTask.Compile.ps1
+++ build/tasks/psakeTask.Compile.ps1
@@ -1,12 +1,18 @@
 Function PSakeTask-Compile {
     # concat one large module file out of single files
     Write-Verbose -Message 'Creating psm1...'
-    $psm1 = Copy-Item -Path (Join-Path -Path $srcDir -ChildPath "$($moduleName).psm1") -Destination $outputModDir -PassThru
+    # starting with Header as base of psm1 for module
+    $psm1 = Copy-Item -Path (Join-Path -Path $srcPublicDir -ChildPath "Header.ps1") -Destination "$outputModDir\$($ModuleName).psm1" -PassThru
 
+    # fetching all public functions and adding to psm1 file
+    Get-ChildItem -Path $srcPublicDir -Recurse -Exclude @("Header.ps1","Footer.ps1") | Get-Content -Raw | Add-Content -Path $psm1 -Encoding UTF8
+    # fetching all private functions and adding to psm1 file
     Get-ChildItem -Path $srcPrivateDir -Recurse | Get-Content -Raw | Add-Content -Path $psm1 -Encoding UTF8
-    Get-ChildItem -Path $srcPublicDir -Recurse | Get-Content -Raw | Add-Content -Path $psm1 -Encoding UTF8
+    # fetching footer and adding to psm1 file
+    Get-ChildItem -Path (Join-Path -Path $srcPublicDir -ChildPath "Footer.ps1") -Recurse | Get-Content -Raw | Add-Content -Path $psm1 -Encoding UTF8
 
+    # adding manifest file to build output
     Copy-Item -Path $manifestFile -Destination $outputModDir
 
-    "`tCreated compiled module at [$outputModDir]"
+    "`tCreated compiled module [$psm1]"
 } 
---
+++ src/Public/Header.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+Connects to a given online service of Microsoft.
+
+.DESCRIPTION
+Connects to a given online service of Microsoft.
+One or multiple service names can be chosen. Supports connection handling for
+- Microsoft Online (MSOL) - aka AzureAD v1
+- Exchange Online (EOL)
+- Teams
+- SharePoint Online (SPO)
+- Security and Compliance Center (SCC)
+- Azure ActiveDirectory (AAD) v2
+- Azure Platform (AZ)
+- Skype for Business Online service (S4B)
+
+.PARAMETER Service
+Specifies the service to connect to. May be a list of multiple services to use.
+
+.INPUTS
+None. You cannot pipe objects to Connect-MS365.
+
+.EXAMPLE
+Description: Connect to Microsoft Online
+Connect-MS365 -Service MSOL
+
+.EXAMPLE
+Description: Connect to Microsoft Online and Exchange Online
+Connect-MS365 -Service MSOL,EOL
+
+.EXAMPLE
+Description: Connect to SharePoint Online to connect to MyName-admin.sharepoint.com
+Connect-MS365 -Service SPO -SPOOrgName MyName
+
+.EXAMPLE
+Description: Connect to Security and Compliance Center
+Connect-MS365 -Service SCC
+
+.EXAMPLE
+Description: Connect to Azure ActiveDirectory  
+Connect-MS365 -Service AAD
+
+.EXAMPLE
+Description: Connect to Microsoft Azure platform
+Connect-MS365 -Service AZ
+
+.EXAMPLE
+Description: Connect to Microsoft Skype for Business Online service
+Connect-MS365 -Service S4B
+
+.LINK
+https://github.com/blindzero/Connect-MS365
+
+#>

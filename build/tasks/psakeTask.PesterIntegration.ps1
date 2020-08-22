ae8c96384f424eac92e010f28385f07a995d4a6e
Matthias Fleschütz <13959569+blindzero@users.noreply.github.com>
2020-08-22 10:46:27 +0200

    change to pester v5 configuration interface object

--- build/tasks/psakeTask.PesterIntegration.ps1
+++ build/tasks/psakeTask.PesterIntegration.ps1
@@ -8,12 +8,25 @@ Function PSakeTask-PesterIntegration {
     if ( $env:PSModulePath.Split($pathSeparator) -notcontains $outputDir ) {
         $env:PSModulePath = ($outputDir + $pathSeparator + $origModulePath)
     }
-
+    # Cleanup module if loaded
     Remove-Module -Name $moduleName -ErrorAction SilentlyContinue -Verbose:$false
+    # Import Module to be tested
     Import-Module -Name $outputModDir -Force -Verbose:$false
+    # set path for test results output
     $testResultsXml = Join-Path -Path $outputDir -ChildPath 'testResultsIntegration.xml'
+    # construct pester configuration object
+    $PesterConfiguration = [PesterConfiguration]::Default
+    $PesterConfiguration.Run.Path = $testsDir
+    $PesterConfiguration.Run.PassThru = $True
+    $PesterConfiguration.Filter.Tag = 'Integration'
+    $PesterConfiguration.Should.ErrorAction = 'Continue'
+    $PesterConfiguration.TestResult.Enabled = $true
+    $PesterConfiguration.TestResult.OutputPath = $testResultsXml
+
+    # goto output directory of module built
     $null = Set-Location -PassThru $outputModDir
-    $testResults = Invoke-Pester -Path $testsDir -Tag Integration -PassThru -OutputFile $testResultsXml -OutputFormat NUnitXML
+    # invoke real testing based on configuration object
+    $testResults = Invoke-Pester -Configuration $PesterConfiguration
 
     if ($testResults.FailedCount -gt 0) {
         $testFailedCount = $testResults.FailedCount

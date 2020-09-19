71dce84fb91252591cd7f138374d4d4517fefb52
Matthias Fleschuetz <13959569+blindzero@users.noreply.github.com>
2020-09-19 21:12:42 +0200

    added inline comments

--- build.ps1
+++ build.ps1
@@ -17,25 +17,35 @@ param (
 
 $ErrorActionPreference = 'Stop'
 
+# start bootstrapping when switch is set
 if ($Bootstrap.IsPresent) {
     $null = Get-PackageProvider -Name Nuget -ForceBootstrap
+    # add PSGallery as repository to install packages
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    # test if PSDepend is not available...
     if (!(Get-Module -Name PSDepend -ListAvailable)) {
+        # and install PSDepend as it will handle all other installations
         Install-Module -Name PSDepend -Scope CurrentUser -Repository PSGallery
     }
+    # import PSDepend separately after installing
     Import-Module -Name PSDepend -Verbose:$false
+    # install all other dependencies by PSDepend
     Invoke-PSDepend -Path '.\build\dependencies.psd1' -Install -Import -Force -Recurse:$true -WarningAction SilentlyContinue
+    # and then install as we do not do anything else
     exit 0
 }
 
-# run psake itself
+# run psake for building itself
 
 # setup build environment with BuildHelpers and manual variables
 Write-Verbose "Invoke BuildHelpers"
+# get buildhelper environment
 Set-BuildEnvironment -Force
 
+# set build dir from buildhelpers project path (base) + build
 $buildDir           = Join-Path -Path $env:BHProjectPath -ChildPath 'build'
 Write-Verbose "Using `$buildDir: $buildDir"
+# set build task dir from buildhelpers project path (base) + tasks
 $buildTasksDir      = Join-Path -Path $buildDir -ChildPath 'tasks'
 Write-Verbose "Using `$buildTasksDir: $buildTasksDir"
 
@@ -51,7 +61,7 @@ Write-Verbose "Using `$psakeProperties: $psakePropertiesFile"
 $taskFiles = (Get-Item -Path (Join-Path -Path $buildTasksDir -ChildPath '*.ps1'))
 Write-Verbose "Found `$taskFiles: $taskFiles"
 
-# assemling properties array to be passed to psake
+# assemling properties array to be passed to psake from all variables before
 $psakeParameters = @{
     "buildDir" = $buildDir;
     "buildTasksDir" = $buildTasksDir;
